aac67d441e9a656cc500a33e488a2686962389f6
navmanak <iit2019066@iiita.ac.in>
2024-10-08 22:31:01 +0530

    navu

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'https://graphql-backend-eoym.onrender.com/graphql',
   cache: new InMemoryCache(),
 });
 
